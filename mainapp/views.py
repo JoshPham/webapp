a45351aa4f4d3a65c6ae60f9d098bf5d66fedc94
JoshPham <JTPham911@gmail.com>
2023-02-03 20:35:06 -0800

    added urls, modified views, changed settings to incorporate all files and connect them to the app

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {
+        'title': "Home",
+    }
+    return render(request, 'mainapp/home.html', context)
