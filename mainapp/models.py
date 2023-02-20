6ec2d3d7bc8d689ea0487aa4381d17b810414ddc
JoshPham <JTPham911@gmail.com>
2023-02-19 22:36:41 -0800

    styled website and added posting feature

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-updated", "-created"]
+    
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.text[0:50]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
 
 def home(request):
+    posts = Post.objects.all()
     context = {
         'title': "Home",
+        'posts': posts,
     }
     return render(request, 'mainapp/home.html', context)
+
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {
+        'title': post.title,
+        'post': post,
+    }
+    return render(request, 'mainapp/post.html', context)
+
+def createPost(request):
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context = {
+        'title': "Create Post",
+        'form': form
+    }
+    return render(request, 'mainapp/post_form.html', context)
